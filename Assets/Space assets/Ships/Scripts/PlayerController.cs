8e2a65e19eae1ae1dd6fb94c5abe590bd9f8814a
ThargoN <thargonsg@gmail.com>
2016-04-17 01:22:01 +0300

    Small update
    Still working on SpaceCraft refactoring.

--- Assets/Space assets/Ships/Scripts/PlayerController.cs
+++ Assets/Space assets/Ships/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 using System.Collections;
 using System;
 
@@ -15,13 +16,16 @@ namespace Spacecraft {
 		private bool        _isDamping      = false;
 		private float       _dampSpeed      = 10f;
 
+		private Spacecraft_Generic  _ship;
+
 
 
 		// Unity callbacks ////////////////////////////////
 		// Use this for initialization
 		void Start() {
-
-		}
+			_ship = GetComponentInParent<Spacecraft_Generic>();
+			Assert.IsNotNull(_ship, "PlayerController::Start > No parent Spacecraft_Generic found!");
+		}	
 
 		// Update is called once per frame
 		void Update() {
@@ -29,9 +33,21 @@ namespace Spacecraft {
 			if (Input.GetKeyUp(KeyCode.F1)) {
 				swithCamera();
 			}
+
+			//Читаем устройства ввода
+			if (GameController.instance.isCursorLocked) {
+				_ship.setShipControls(	Input.GetAxis("Horisontal Strafe") * _ship.rotationXForce,        // direction x
+										Input.GetAxis("Vertical Strafe") * _ship.rotationYForce,          // direction y
+										Input.GetAxis("Speed") * _ship.maxMainEngineForce,                // direction z
+										Input.GetAxis("Vertical") * _ship.rotationXForce,                 // rotation x
+										Input.GetAxis("Horizontal") * _ship.rotationYForce,               // rotation y
+										Input.GetAxis("Roll") * _ship.rotationZForce);                     // rotation z
+			}
+		 
 		}
 
 		void FixedUpdate() {
+			// если это перенести в LateUpdate, то камера адово дергается
 			if (_isDamping) {
 				cameraToUse.transform.position = Vector3.Lerp(cameraToUse.transform.position, cameraPositions[cameraIndex].position, Time.smoothDeltaTime * _dampSpeed);
 				cameraToUse.transform.rotation = cameraPositions[cameraIndex].rotation;
@@ -39,6 +55,7 @@ namespace Spacecraft {
 		}
 
 		void LateUpdate() {
+			// а если это перенести в FixedUpdate, то камера "плавает", как будто пружинящая (видимо, позиции трансформов еще не обновляются к тому моменту)
 			if (!_isDamping) {
 				cameraToUse.transform.position = cameraPositions[cameraIndex].position;
 				cameraToUse.transform.rotation = cameraPositions[cameraIndex].rotation;
---
+++ Assets/Space assets/Ships/Scripts/Spacecraft_Generic.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace Spacecraft {
+
+	/// <summary>
+	/// Generic spacecraft class
+	/// </summary>
+	public class Spacecraft_Generic : MonoBehaviour, ISpaceEntity {
+
+
+		public  GameObject  engineLights;
+
+		public  bool        controlledByPlayer  = false;
+		public  string      shipName            = "Test Ship";
+		// Текущие характеристики конкретного корабля (не общие для класса, т.е. с учетом всех апгрейдов и т.п.)
+
+		[Header("Physics")]
+		public  float       nominalMass         = 10f;  //Номинальная масса корабля (пустой корабль, тонны)
+		public  float       inertiaDamper       = 1f;   //Эффективность гасителя инерции (0.1 - 5)
+		public  float       maxMainEngineForce  = 100f; //Максимальная мощность маршевого двигателя, kN (килоньютоны)
+		public  float       rotationXForce      = 40f;  //Мощность поворотных двигателей (она же половина мощности стрейфа) (ось X, вертикальная, kN (килоньютоны))
+		public  float       rotationYForce      = 30f;  //Мощность поворотных двигателей (она же половина мощности стрейфа) (ось Y, горизонтальная, kN (килоньютоны))
+		public  float       rotationZForce      = 20f;  //Мощность поворотных двигателей (ось Z, ось движения, kN (килоньютоны))
+
+		float[]     forces              = {0f, 0f, 0f, 0f, 0f, 0f};
+
+		private string      _guid;
+
+		public string GUID {
+			get { return _guid; }
+		}
+
+		public string e_name {
+			get { return shipName; }
+		}
+
+		// Use this for initialization
+		void Awake() {
+
+			//prepare cameras
+			FindObjectOfType<PlayerController>().initialiseCameras();
+
+			setupPhysics();
+			setupGUID();
+			gameObject.name = shipName + " (" + GUID + ")";
+		}
+
+		static int ships;
+		protected void setupGUID() {
+			//classType = ClassTypes.ship;
+			_guid = "XXXX-SHIP-000" + ++ships;
+		}
+
+
+
+		// Update is called once per frame
+		void Update() {
+
+						
+			float speedFactor = Vector3.Dot(GetComponent<Rigidbody>().velocity, transform.forward) / 26f;
+			var localVelocity = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
+			float forwardSpeed = Mathf.Max(0, localVelocity.z);
+
+			
+			//TODO: Выпилить все это в отдельный модуль, который будет кешировать все нужные ссылки и в нужные моменты включать нужные эффекты
+			if (engineLights) {
+				foreach (Light l in engineLights.GetComponentsInChildren<Light>()) {
+					l.intensity = Mathf.Abs(Input.GetAxis("Speed"));
+				}
+			}
+
+			//TODO: перенести в управление эффектами движков
+			if (forces[2] > 0.1f) {
+				foreach (ParticleSystem p in GetComponentsInChildren<ParticleSystem>(true)) {
+					ParticleSystem.EmissionModule em = p.emission;
+					em.enabled = true;
+				}
+			} else {
+				foreach (ParticleSystem p in GetComponentsInChildren<ParticleSystem>(true)) {
+					ParticleSystem.EmissionModule em = p.emission;
+					em.enabled = false;
+				}
+			}
+						
+			////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		}
+
+
+
+		void FixedUpdate() {
+			// Applying our forces
+			GetComponent<Rigidbody>().AddRelativeForce(forces[0], forces[1], forces[2], ForceMode.Force);
+			GetComponent<Rigidbody>().AddRelativeTorque(forces[3], forces[4], forces[5], ForceMode.Force);
+		}
+
+		public void setShipControls(float hStrafe, float vStrafe, float forward, float pitch, float yaw, float roll) {
+			forces[0] = hStrafe;
+			forces[1] = vStrafe;
+			forces[2] = forward;
+			forces[3] = pitch;
+			forces[4] = yaw;
+			forces[5] = roll;
+		}
+
+		protected void setupPhysics() {
+			//setup the rigidbody
+			if (!GetComponent<Rigidbody>()) {
+				gameObject.AddComponent<Rigidbody>();
+			}
+			GetComponent<Rigidbody>().mass = nominalMass;
+			GetComponent<Rigidbody>().useGravity = false;
+			GetComponent<Rigidbody>().drag = inertiaDamper;
+			GetComponent<Rigidbody>().angularDrag = inertiaDamper * 2f;
+		}
+
+	}
+}
