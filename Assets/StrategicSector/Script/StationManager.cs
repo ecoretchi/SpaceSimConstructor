4e7371b8958830ee2e0cd8afe6b875995f40b212
Evgeny Koretsky <e.koretsky@octavianonline.com>
2016-04-25 18:03:30 +0300

    slight update

--- Assets/StrategicSector/Script/StationManager.cs
+++ Assets/StrategicSector/Script/StationManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class StationManager : MonoBehaviour {
 
+	//station characteristics
+	StationCharacteristics m_stationInfo;
     MainStationModule m_cms;
     // Use this for initialization
     void Start () {
@@ -19,3 +21,27 @@ public class StationManager : MonoBehaviour {
 	
 	}
 }
+
+//http://answers.unity3d.com/questions/901770/invoke-method-with-reflection-c.html
+
+public class StationCharacteristics {
+	public int theHull {get;set;}
+	public int hullWeakening {get;set;}
+	public double energyGWatts {get;set;}
+	public double maintenance {get;set;}
+	public double civilLoyalty {get;set;}
+
+	public int transportRoutesMax {get;set;}
+	public int transportRoutes {get;set;}
+
+	public double pureApartment {get;set;}
+	public double normalApartment {get;set;}
+	public double luxApartment {get;set;}
+
+	// ========== demands ========== 
+	public double pureApartmentDemands {get;set;}
+	public double normalApartmentDemands {get;set;}
+	public double luxApartmentDemands {get;set;}
+
+}
+
