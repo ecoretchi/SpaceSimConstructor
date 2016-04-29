d40694699576610dc6e1d3593348c8e007c2e2dc
Evgeny Koretsky <e.koretsky@octavianonline.com>
2016-04-29 17:56:57 +0300

    station

--- Assets/StrategicSector/Script/StationManager.cs
+++ Assets/StrategicSector/Script/StationManager.cs
@@ -27,93 +27,116 @@ public class StationData {
 	public double lifeSupport;
 	public double credits;
 	public double freelanceTaxFactor;
+	public double freelanceTaxCreditRate;//credit tax rate
+
 	public double socialApartments;
 	public double conventionalApartments;
 	public double luxApartments;
 
-	public double stuffWorkers;
-	public double freelanceWorkers;
-	public double luxCivil;
+	public double stuffWorkers; // citizen for maintenance
+	public double freelanceWorkers; // citizen for freelance
+	public double luxCivil; // VIP citizen for high busness
 
-	public double maintenanceLevel;
-	public double maintenanceFunds;
-	public double maintenanceRequired;
+	public double maintenanceLevel; // current station maintenance level
+	public double maintenanceFunds; // current fund by user 
+	public double maintenanceRequired; // station need maintenance at now
+
+	protected double stuffWorkersFactorCome = 1;
+	protected double freelanceWorkersFactorCome = 0.75f;
+	protected double luxCivilFactorCome = 0.25f;
+
+	public int wantComeStuffWorkers;
+	public int wantComeFreelanceWorkers;
+	public int wantComeLuxCivil;
+
+	public int newAcceptedStuffWorkers;
+	public int newAcceptedFreelanceWorkers;
+	public int newAcceptedLuxCivil;
 
 
 	//civil like this station
-	public double GetStationAppeal () {
-		//(2-O7/E7-O7/P7)*(W7/200)
-		double O7 = GetCivil();
-		double E7 = GetLifeSupportFree();
-		double P7 = GetApartmentsTotal();
-		double W7 = maintenanceLevel;
-		return (2-O7/E7-O7/P7)*(W7/200);
+	public double CalcStationAppeal (int minLevel = 50) {
+		double O7 = CalcCivil();
+		double E7 = CalcLifeSupportFree();
+		double P7 = CalcApartmentsTotal();
+		double W7 = maintenanceLevel-minLevel;
+		return (4-O7/E7-O7/P7)*W7/400;
 	}
-	public int GetLifeSupportFree(){ 
-		double O7 = GetCivil();
-		double E7 = GetLifeSupportFree();
+	public int CalcLifeSupportFree(){ 
+		double O7 = CalcCivil();
+		double E7 = CalcLifeSupportFree();
 		return E7-O7;
 	}
-	public double GetTaxProfit(){ 
-		//TODO: 
-		return 0;}
-	public int GetCivil(){ 
+	public double CalcTaxProfit(){ 
+		return freelanceTaxFactor * freelanceTaxCreditRate;
+	}
+	public int CalcCivil(){ 
 		return stuffWorkers + freelanceWorkers + luxCivil;
 	}
-	public int GetApartmentsTotal(){ 
-		//TODO: 
-		return 0;
+	public int CalcApartmentsTotal(){ 
+	 return socialApartments + 
+	 	conventionalApartments +
+	 	luxApartments;
 	}
-
-	public double GetMaintenanceConsume(){ 
-		//TODO: 
-		return 0;
+	public double CalcMaintenanceConsume(){ 
+		double AB = maintenanceFunds;
+		double AC = CalcMaintenanceAvail();
+		double AD = maintenanceRequired;
+		double res = AB*((AC-AD)/5+AD);
+		return res>0?res:0;
 	}
-
-	public double GetMaintenanceAvail(){ 
-		//TODO: 
-		return 0;
+	public double CalcMaintenanceAvail(){ 
+		double W = stuffWorkers;
+		return (int)(W/10);
 	}
-
-	public double GetMaintenancePerformance(){ 
-		//TODO: 
-		return 0;
+	public double CalcMaintenancePerformance(){ 
+		double AB = maintenanceFunds;
+		double AC = CalcMaintenanceAvail();
+		double AD = maintenanceRequired;
+		return AC/AD*AB/1000.0f-1.05f;
 	}
-	public double GetMaintenanceFree(){ 
-		//TODO: 
-		return 0;
+	public double CalcMaintenanceFree(){ 
+		double AC = CalcMaintenanceAvail();
+		double AD = maintenanceRequired;
+		return AC-AD;
 	}
 	//---- want come ---
-	public int GetWantComeStuffWorkers(){ 
-		//TODO: 
-		return 0;
+	public int CalcWantComeStuffWorkers(){ 
+		double C7 = CalcStationAppeal(50);
+		double freeRooms = socialApartments - stuffWorkers;
+		wantComeStuffWorkers = stuffWorkersFactorCome * C7 * freeRooms;
+		return wantComeStuffWorkers;
+	 	return ;
 	}
-	public int GetWantComeFreelanceWorkers(){ 
-		//TODO: 
-		return 0;
+	public int CalcWantComeFreelanceWorkers(){ 
+		double C7 = CalcStationAppeal(60);
+		double freeRooms = conventionalApartments - freelanceWorkers;
+		wantComeFreelanceWorkers =  freelanceWorkersFactorCome * C7 * freeRooms;	 
+		return wantComeFreelanceWorkers;
 	}
-	public int GetWantComeLuxCivil(){ 
-		//TODO: 
-		return 0;
+	public int CalcWantComeLuxCivil(){ 
+		double C7 = CalcStationAppeal(70);
+		double freeRooms = luxApartments - luxCivil;
+		wantComeLuxCivil = luxCivilFactorCome * C7 * freeRooms;
+	 	return wantComeLuxCivil;
 	}
-	public int GetIncomeCivil(){ 
-		return GetWantComeStuffWorkers() + GetWantComeFreelanceWorkers() + GetWantComeLuxCivil();
+	public int CalcWantComeCivil(){ 
+		return CalcWantComeStuffWorkers() + CalcWantComeFreelanceWorkers() + CalcWantComeLuxCivil();
 	}
+	protected void CalcNewAcceptedCivil(){ 
+		int H = CalcLifeSupportFree();
+		double AH = CalcWantComeStuffWorkers();
+		double AJ = CalcWantComeFreelanceWorkers();
+		double AL = CalcWantComeLuxCivil();
+		double heft = AH+AJ+AL;
+		double AH_f = AH/heft;
+		double AJ_f = AJ/heft;
+		double AL_f = AH/heft;
 
-	//---- new accepted civil---
-	public int GetNewStuffWorkersAccepted(){ 
-		//TODO: 
-		return 0;
+		newAcceptedStuffWorkers = AH > H ? AH: H*AH_f;
+	  	newAcceptedFreelanceWorkers = AJ > H ? AJ: H*AJ_f;
+	  	newAcceptedLuxCivil = AL > H ? AL: H*AL_f;
 	}
-	public int GetNewFreelanceWorkersAccepted(){ 
-		//TODO: 
-		return 0;
-	}
-	public int GetNewLuxCivilAccepted(){ 
-		//TODO: 
-		return 0;
-	}
-
 }
 //http://answers.unity3d.com/questions/901770/invoke-method-with-reflection-c.html
 
